b7d8b017ae4f5bea7df5b2baa3ea6adf260a1bf1
Michal Czyz <michalczyz@gmail.com>
2013-11-18 17:17:01 +0100

    adding some future validation

--- app/models/work/unit.rb
+++ app/models/work/unit.rb
@@ -1,4 +1,9 @@
 # * should have uid and aliases
+# * uniqueness of wuid
+#   => customer within company
+#   => project within customer
+#   => phase have no wuid
+#   => all children within phase
 class Work::Unit
   has_many :time_entries, class_name: 'Work::TimeEntry'
 end
