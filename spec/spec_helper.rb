e4fce56570d7b5a9de3f4dacefd2b803100e00c7
Michal Czyz <michalczyz@gmail.com>
2013-11-18 15:33:47 +0100

    reconfiguring spec helper & updating Gems

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,6 +38,13 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  # focus specs
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+  # and fail fast
+  config.fail_fast = false
   
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
