624c4093195c65fb9c8275f91f2e1bf7d0088e87
Michal Czyz <michalczyz@gmail.com>
2013-11-18 17:18:31 +0100

    first stage of parsing time entries from text

---
+++ app/services/work/time_entries_import_from_text.rb
@@ -0,0 +1,59 @@
+# It converts text lines in formats
+#
+#    2013-06-04	11:00	11:15	|	selleo-858
+#    2013-06-04	12:15	13:30	|	metreno-853
+#    2013-06-04	13:30	14:15	|	hrm - feedback for WR
+#    2013-06-04	14:15	14:30	|	sourcyx-742
+#    2013-06-04	14:30	14:45	|	selleo-760
+#
+# Into Array of Hashes, e.g:
+# [
+#   {
+#     data:             '2013-06-04',
+#     start_at:         '13:30,'
+#     end_at:           '14:15',
+#     workload_object:  <project::hrm>
+#     comment:          'feedback for WR'
+#   },
+#   ...
+# ]
+#
+# If any line is unprocessable the it raise exception with line number and content
+
+class Work::TimeEntriesImportFromText
+
+  def self.parse(text)
+    text.strip.split("\n").collect do |text_line|
+      SingleLine.new(text_line).attrs
+    end
+  end
+
+  # To change this template use File | Settings | File Templates.
+
+  class SingleLine < Struct.new(:text_line)
+    def attrs
+      {
+          date: match[:date],
+          start_at: match[:start_at],
+          end_at: match[:end_at],
+          work_unit: work_unit,
+          comment: match[:comment]
+      }
+    end
+
+    private
+
+    def match
+      time_entry_line_regex.match(text_line)
+    end
+
+    def work_unit
+      nil
+    end
+
+    def time_entry_line_regex
+      /^(?<date>[^\s]{10})\s+(?<start_at>[^\s]{5})\s+(?<end_at>[^\s]{5})\s+(?<context>[^\s]+)[^(\n)](?<comment>[^(\n)]+)?$/
+    end
+  end
+end
+
--- spec/acceptance/importing_work_time_entries_spec.rb
+++ spec/acceptance/importing_work_time_entries_spec.rb
@@ -1,19 +1,40 @@
 require 'spec_helper'
 
-describe "Importing Work Time Entries" do
+describe "Importing Work Time Entries", :focus do
   let(:tab_separated_time_entries) {
-    %Q{
-2013-11-18	08:30	09:45	|	sourcyx-manage
-2013-11-13	16:00	17:00	|	hrm - coworkers communication
-2013-11-11	09:00	09:45	|	process_and_tools
-2013-11-05	15:00	16:30	|	mikstura.it - topics / blocks
-2013-09-02	10:45	11:00	|	sourcyx-ext::spd-117
-2013-09-15	09:30	17:30	|	selleo-1514 working merit money pay
+    %q{
+2013-11-18	08:30	09:45	sourcyx-manage
+2013-11-13	16:00	17:00	hrm - coworkers communication
+2013-11-11	09:00	09:45	process_and_tools
+2013-11-05	15:00	16:30	mikstura.it - topics / blocks
+2013-09-02	10:45	11:00	sourcyx-ext::spd-117 writing specs
+2013-09-15	09:30	17:30	selleo-1514 working merit money pay
     }
   }
 
-  let(:perform) {
-
+  let(:time_entries) {
+    Work::TimeEntriesImportFromText.parse(tab_separated_time_entries)
   }
+
+  let(:sourcyx_manage_time_entry) { time_entries.first }
+  pending "work unit unfinished" do
+    let(:sourcyx_manage_work_unit) { Work::Unit.where(wuid: 'sourcyx-manage') }
+  end
+  it { expect(sourcyx_manage_time_entry).to eq({
+                                                   date: '2013-11-18',
+                                                   start_at: '08:30',
+                                                   end_at: '09:45',
+                                                   work_unit: nil,
+                                                   comment: nil
+                                               }) }
+
+  let(:sourcyx_spd_117_time_entry) { time_entries[4] }
+  it { expect(sourcyx_spd_117_time_entry).to eq({
+                                                    date: '2013-09-02',
+                                                    start_at: '10:45',
+                                                    end_at: '11:00',
+                                                    work_unit: nil,
+                                                    comment: 'writing specs'
+                                                }) }
 
 end
