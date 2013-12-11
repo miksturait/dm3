e8e33c1e745711844745f5b16c75c8c04aac5fa2
Michal Czyz <michalczyz@gmail.com>
2013-12-11 20:17:00 +0100

    refactoring - readability improvements

--- app/services/work/unit_structure_import/recreate_based_on_work_unit_context.rb
+++ app/services/work/unit_structure_import/recreate_based_on_work_unit_context.rb
@@ -1,18 +1,22 @@
 class Work::UnitStructureImport::RecreateBasedOnWorkUnitContext < Struct.new(:phase, :work_units_context)
   def process
-    find_or_create_child(phase, next_child)
+    find_or_create_child(phase, next_child_work_unit_context)
   end
 
   def find_or_create_child(parent, work_unit_context)
     return nil if work_unit_context.nil?
-    parent.children.where(wuid: work_unit_context.wuid,
-                 name: work_unit_context.name).first_or_create.
-        tap { |potential_parent| find_or_create_child(potential_parent, next_child) }
+    parent.children.where(
+        wuid: work_unit_context.wuid,
+        name: work_unit_context.name).
+
+        first_or_create.
+
+        tap { |potential_parent| find_or_create_child(potential_parent, next_child_work_unit_context) }
   end
 
   private
 
-  def next_child
+  def next_child_work_unit_context
     work_units_context_reversed.pop
   end
 
