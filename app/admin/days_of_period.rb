f335e9386406060b51202390b2ff78dd0246b482
Michal Czyz <michalczyz@gmail.com>
2014-01-03 20:06:38 +0100

    DaysOffPeriod - active admin editing

--- app/admin/days_of_period.rb
+++ app/admin/days_of_period.rb
@@ -1,3 +1,4 @@
 ActiveAdmin.register Work::DaysOffPeriod, as: "Days Off" do
   menu parent: "Schedule", priority: 4
+  permit_params :coworker_id, :comment, :period
 end
--- app/models/work/days_off_period.rb
+++ app/models/work/days_off_period.rb
@@ -4,4 +4,6 @@ class Work::DaysOffPeriod < ActiveRecord::Base
   scope :official_days_off_and_days_of_for_coworker, ->(coworker) { where(coworker_id: [coworker.id, nil]) }
   scope :official_days_off, -> { where(coworker_id: nil) }
   scope :within_period, ->(range) { where(["period && daterange(?,?, '[]')", range.begin.to_s, range.end.to_s]) }
+
+  delegate :begin, :end, to: :period, prefix: true, allow_nil: true
 end
