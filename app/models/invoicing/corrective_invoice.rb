1d5554173d2cc54f78aeee51bc139af45c3fbd67
Michal Czyz <michalczyz@gmail.com>
2014-01-06 22:38:56 +0100

    fix

--- app/models/invoicing/corrective_invoice.rb
+++ app/models/invoicing/corrective_invoice.rb
@@ -2,5 +2,5 @@
 # duplicate of the invoice with a link to an original invoice
 # duplicating process should be done in separate service object :-)
 # after that original invoice have value zero
-class CorrectiveInvoice < Invoice
+class CorrectiveInvoice #< Invoice
 end
