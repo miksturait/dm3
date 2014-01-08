ed2de97583cf775a6cbe259616833ab3ed859d39
Michal Czyz <michalczyz@gmail.com>
2014-01-08 20:05:52 +0100

    improvement to capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,6 @@ namespace :deploy do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
       # execute :touch, release_path.join('tmp/restart.txt')
-
       run <<-CMD
       if [[ -f #{current_path}/tmp/pids/passenger.pid ]];
       then
