86390063d54f45a9a46fcd0187f3b2cc3c54312f
Michal Czyz <michalczyz@gmail.com>
2014-02-02 17:13:03 +0100

    jira export worklog

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ gem 'simple_form', '>= 3.0.0.rc'
 gem 'ancestry'
 gem 'time_diff'
 gem 'youtrack_api', git: 'https://github.com/eisnerd/youtrack-rest-ruby-library.git'
+gem 'httparty'
 gem "active_model_serializers"
 gem "select2-rails"
 
---
+++ spec/services/jira_export/workload_spec.rb
@@ -0,0 +1,16 @@
+# curl -u t.borowski@selleo.com:s3ll3o -v -H "Accept: application/json" -H "Content-type: application/json" -X POST \
+#   -d "{ \"comment\": \"test api\", \"started\": \"2014-01-02T11:50:00.000+0000\", \"timeSpent\": \"10m\" }" \
+#   https://vtelligence.atlassian.net/rest/api/2/issue/SPD-566/worklog\?adjustEstimate\=auto
+
+
+class Jira
+
+end
+
+describe JiraExport::Workload do
+
+  # example time entry for example project
+
+  # have an service class
+
+end
