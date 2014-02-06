4abed03b6eb8b27d241cde4b1aceb4c1f486f102
Michal Czyz <michalczyz@gmail.com>
2014-02-06 11:03:59 +0100

    project edit

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Project do
   menu parent: "Work Unit's", priority: 3
+  permit_params :wuid, :name, :opts
+
+  form do |f|
+    f.inputs do
+      f.input :wuid, as: :string
+      f.input :name, as: :string
+      f.input :opts, as: :string
+    end
+
+    f.actions
+  end
+
 end
