baf275031d627d39f75ea09b7ff85db861dc86e7
Szymon Kieloch <s.kieloch@selleo.com>
2014-02-21 06:03:19 +0100

    [Cleanup] Reorder code within the project model.

--- app/models/dm2/project.rb
+++ app/models/dm2/project.rb
@@ -2,14 +2,14 @@ module Dm2
   class Project < Dm2::Base
     has_many :children, class_name: self, foreign_key: :parent_id
 
+    scope :roots, -> { where(parent_id: nil) }
+
     def descendants
-      self.class.where(["lft >= ? AND rgt <= ?", lft, rgt])
+      self.class.where(['lft >= ? AND rgt <= ?', lft, rgt])
     end
 
     def time_entries
       Dm2::TimeEntry.where("project_id IN (#{descendants.select(:id).to_sql})")
     end
-
-    scope :roots, -> { where(parent_id: nil) }
   end
 end
