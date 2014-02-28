8c26d0b860f1f60f6302525b789fd9dcd0da5e93
stevo <blazejek@gmail.com>
2014-02-28 15:02:05 +0100

    reduce filter fields for jira exports

--- app/admin/jira_export.rb
+++ app/admin/jira_export.rb
@@ -1,6 +1,9 @@
 ActiveAdmin.register Jira::Export do
   menu parent: "Work Unit's", priority: 8
 
+  filter :last_error
+  filter :processed_at
+
   controller do
     def new
       Jira::TimeEntriesExport.new.perform
