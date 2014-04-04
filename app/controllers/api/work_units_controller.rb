0cccb995425ca68bdcf0b38cb787066d2013c771
bart <wojtowicz.bartlomiej@gmail.com>
2014-04-04 13:01:25 +0200

    work units api

--- app/controllers/api/work_units_controller.rb
+++ app/controllers/api/work_units_controller.rb
@@ -6,6 +6,30 @@ class Api::WorkUnitsController < Api::ApplicationController
   private
 
   def collection
-    Work::Unit.order(:id).to_json(root: false)
+    scope = Work::Unit.order(:id)
+    if work_unit_params[:parent_work_unit_id].present?
+      parent = Work::Unit.find(work_unit_params[:parent_work_unit_id])
+      scope = scope.children_of(parent)
+    end
+    scope = scope.less_or_equal_depth(work_unit_params[:depth]) if work_unit_params[:depth].present?
+    scope.to_json(
+      root: false,
+      only: [
+        :id,
+        :wuid,
+        :name,
+        :ancestry,
+        :type,
+        :created_at,
+        :updated_at,
+        :period,
+        :opts,
+        :ancestry_depth
+      ]
+    )
+  end
+
+  def work_unit_params
+    @work_unit_params ||= params.permit(:parent_work_unit_id, :depth)
   end
 end
