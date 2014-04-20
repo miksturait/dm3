b1a9a84bb7fb54a8074346d8c641e0369da412e3
Michal Czyz <michalczyz@gmail.com>
2014-04-20 12:40:08 +0200

    plan for development :-) [bonus 2x]

--- spec/controllers/dm2/api_controller_spec.rb
+++ spec/controllers/dm2/api_controller_spec.rb
@@ -44,6 +44,28 @@ describe Dm2::ApiController do
     xit "dashboard with info about hours diff (worked vs bought) in dm2"
   end
 
+  context "Bonus :-) :: Allow to create Target Together with creating Invoice", :focus do
+    xit "extend the push api"
+
+    xit "create service object that handle this use case (creating muliple objects"
+
+    xit "display all targets withing the invoice (show / edit)"
+
+    xit "add interface in dm2 that will allow create one target per invoice update / create"
+  end
+
+  context "Bonus :: Bonus :: Visualize Targets", :focus do
+    # http://visjs.org/examples/timeline/02_dataset.html
+    # or
+    # Google Calendar Integration
+    # One Calendar per co-worker / one per client
+    xit "rething strategy how"
+
+    # * just one calender "Schedule" where all the objects are read only
+    #   and within details we would be inviting certain people to add it to their own calendar
+    xit "if google calender - rething how it should works"
+  end
+
   describe "GET 'workload_import'" do
     context "failure" do
       let!(:call) {
